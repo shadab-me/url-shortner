b068277d8c4d64dae5c534a213d61e9238704f4b
Shadab Ali <shadabali604@gmail.com>
2021-01-21 17:17:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => {
+  res.send('<h1>Under Devlopment </h1>');
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
